685a5e7cb4adede15f4e9d0bd11ee6d5421e721f
Pablo Ariata <pabloariata@gmail.com>
2016-06-23 19:47:08 -0300

    va el clienteMapper

--- src/Mappers/clienteMapper.java
+++ src/Mappers/clienteMapper.java
@@ -8,34 +8,96 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Modelo.Cliente;
-import Modelo.Vehiculo;
 
-public class clienteMapper extends baseMapper {
 
+
+
+
+public class clienteMapper extends baseMapper{
+
+	
+		public static clienteMapper instancia;
+	
+	// SINGLETON
+		public static clienteMapper getInstance(){
+			
+			if (instancia==null)
+				instancia = new clienteMapper();
+			
+			return instancia;
+			
+		}
 	
 	public List<Cliente> SelectAll(){
 		
-		List<Cliente> listaClientes = new ArrayList<Cliente>();
+		List<Cliente> listaclientes = new ArrayList<Cliente>();
 		Connection con = null;
 		
-		try {
-			con = Conectar();
+		
+
+			try {
+				con = Conectar();
 			
-			String senten = "SELECT * FROM cliente";
+			
+			String senten = "SELECT idcliente,dni,tipodni,nombre,telefono,mail,direccion,estado FROM CLIENTE";
 			PreparedStatement ps = null;
 			ps = con.prepareStatement(senten);			
 			ResultSet res = ps.executeQuery();
 			
 			while (res.next()){
 				
+				Cliente c = new Cliente(res.getString("nombre"), res.getString("dni"), res.getString("mail"), res.getString("direccion"), res.getString("tipodni"));
+			
+				listaclientes.add(c);
+				
+			}
+			return listaclientes;
+			} catch (SQLException e) {
+		
+				e.printStackTrace();
+			}
+			return listaclientes;
+			
+	
+			
+		}
+	
+	
+		public Cliente SelectCliente(String DNI, String tipoDoc){
+			
+			Cliente c = new Cliente();
+			
+			Connection con = null;
+			PreparedStatement ps = null;
+			
+			String senten = "SELECT idcliente,dni,tipodni,nombre,telefono,mail,direccion,estado FROM CLIENTE WHERE tipodni = ? AND dni = ?";
+			
+			try {
+				ps = con.prepareStatement(senten);
+			
+			ps.setString(1, tipoDoc);
+			ps.setString(2, DNI);
+			
+			ResultSet res = ps.executeQuery();
+			
+			while (res.next()){
+				
+			c = new Cliente(res.getString("nombre"), res.getString("dni"), res.getString("mail"), res.getString("direccion"), res.getString("tipodni"));
+				
+				
 			}
 			
-		} catch (SQLException e) {
-			e.printStackTrace();
+			return c;
+			
+			} catch (SQLException e) {
+				
+				e.printStackTrace();
+			}
+			return null;
+			
+			
 		}
-		
-		return listaClientes;
-	}
-	
 	
 }
+
+	
