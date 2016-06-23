3c2c411ed9f1c468b747bdc919f521d6a423fda4
camilaibar <camuibar@gmail.com>
2016-06-22 22:03:19 -0300

    Cambie la pantalla

--- src/vista/generarReporteMantenimiento.java
+++ src/vista/generarReporteMantenimiento.java
@@ -21,21 +21,21 @@ public class generarReporteMantenimiento extends JInternalFrame {
 		setBounds(100, 100, 800, 500);
 		getContentPane().setLayout(null);
 		
-		JLabel lblReporteMantenimiento = new JLabel("REPORTE MANTENIMIENTO");
+		JLabel lblReporteMantenimiento = new JLabel("Reporte Mantenimiento");
 		lblReporteMantenimiento.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblReporteMantenimiento.setBounds(269, 25, 250, 59);
+		lblReporteMantenimiento.setBounds(267, 22, 215, 59);
 		getContentPane().add(lblReporteMantenimiento);
 		
 		JLabel lblSucursal = new JLabel("Sucursal");
-		lblSucursal.setBounds(25, 156, 69, 14);
+		lblSucursal.setBounds(249, 137, 69, 20);
 		getContentPane().add(lblSucursal);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(141, 153, 140, 20);
+		comboBox.setBounds(365, 137, 140, 20);
 		getContentPane().add(comboBox);
 		
 		JButton btnGenerarReporte = new JButton("Generar Reporte");
-		btnGenerarReporte.setBounds(333, 264, 148, 23);
+		btnGenerarReporte.setBounds(292, 240, 164, 23);
 		getContentPane().add(btnGenerarReporte);
 
 	}
