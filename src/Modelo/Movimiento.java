28c2b62b3d6c7747fb69403b8274be5fc5568b04
camilaibar <camuibar@gmail.com>
2016-06-23 19:31:16 -0300

    Atributos, getters y setters

--- src/Modelo/Movimiento.java
+++ src/Modelo/Movimiento.java
@@ -1,11 +1,53 @@
 package Modelo;
 
+import java.util.Date;
+
 public class Movimiento {
 
-	private Sucursal origen;
-	private Sucursal destino;
+	private int numero;
+	private Sucursal sucursalOrigen;
+	private Sucursal sucursalDestino;
 	private Vehiculo vehiculo;
+	private Date fechaInicio;
+	private Date fechaFin;
 	
+	//GETTER Y SETTER
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public Sucursal getSucursalOrigen() {
+		return sucursalOrigen;
+	}
+	public void setSucursalOrigen(Sucursal sucursalOrigen) {
+		this.sucursalOrigen = sucursalOrigen;
+	}
+	public Sucursal getSucursalDestino() {
+		return sucursalDestino;
+	}
+	public void setSucursalDestino(Sucursal sucursalDestino) {
+		this.sucursalDestino = sucursalDestino;
+	}
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
 	
 	
 	
