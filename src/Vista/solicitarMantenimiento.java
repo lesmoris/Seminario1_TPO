cbc733e2e0222608a5231b9e4e29c85ece8c351a
Leonardo Esmoris <leonardo.esmoris@axa-assistance.com>
2016-07-05 16:13:06 -0300

    Mantenimiento anda ok ahora

--- src/Vista/solicitarMantenimiento.java
+++ src/Vista/solicitarMantenimiento.java
@@ -43,6 +43,7 @@ public class solicitarMantenimiento extends JInternalFrame {
 		descripcionFallaTF.setBorder(new LineBorder(new Color(0, 0, 0)));
 		descripcionFallaTF.setLineWrap(true);
 		descripcionFallaTF.setBounds(355, 116, 351, 165);
+		descripcionFallaTF.setEnabled(false);
 		getContentPane().add(descripcionFallaTF);
 
 		JLabel lblDominio = new JLabel("Dominio");
@@ -95,13 +96,13 @@ public class solicitarMantenimiento extends JInternalFrame {
 				// Recibo y muestro el resultado
 				if (res.sosExitoso()) {
 
-					lblDescripcion.setEnabled(true);
+					descripcionFallaTF.setEnabled(true);
 					btnConfirmar.setEnabled(true);
 				} else {
 					JOptionPane.showMessageDialog(null, res.getMessage(),
 							"Error", JOptionPane.ERROR_MESSAGE);
 
-					lblDescripcion.setEnabled(false);
+					descripcionFallaTF.setEnabled(false);
 					btnConfirmar.setEnabled(false);
 				}
 			}
