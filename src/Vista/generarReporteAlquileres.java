064a17113afa5a5f9190bf89f492681abfb0bd7d
Leonardo Esmoris <leonardo.esmoris@axa-assistance.com>
2016-07-07 16:51:26 -0300

    Reordeno controles en pantalla de reporte de alquileres

--- src/Vista/generarReporteAlquileres.java
+++ src/Vista/generarReporteAlquileres.java
@@ -12,6 +12,8 @@ import javax.swing.JTextField;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class generarReporteAlquileres extends JInternalFrame {
 	private JTextField textField;
@@ -38,11 +40,11 @@ public class generarReporteAlquileres extends JInternalFrame {
 		getContentPane().add(lblReporteDeAlquileres);
 		
 		JLabel lblNewLabel = new JLabel("Sucursal Origen");
-		lblNewLabel.setBounds(10, 242, 99, 14);
+		lblNewLabel.setBounds(388, 94, 99, 14);
 		getContentPane().add(lblNewLabel);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(119, 239, 76, 20);
+		comboBox.setBounds(492, 116, 96, 20);
 		getContentPane().add(comboBox);
 		
 		JLabel lblFechaDesde = new JLabel("Desde");
@@ -58,7 +60,7 @@ public class generarReporteAlquileres extends JInternalFrame {
 		getContentPane().add(lblFechaInicio);
 		
 		JLabel lblFechaFin = new JLabel("FECHA FIN");
-		lblFechaFin.setBounds(254, 69, 71, 14);
+		lblFechaFin.setBounds(173, 69, 71, 14);
 		getContentPane().add(lblFechaFin);
 		
 		textField = new JTextField();
@@ -72,11 +74,11 @@ public class generarReporteAlquileres extends JInternalFrame {
 		textField_1.setColumns(10);
 		
 		JLabel lblDesde = new JLabel("Desde");
-		lblDesde.setBounds(210, 94, 46, 14);
+		lblDesde.setBounds(173, 94, 46, 14);
 		getContentPane().add(lblDesde);
 		
 		JLabel lblHasta = new JLabel("Hasta");
-		lblHasta.setBounds(210, 119, 46, 14);
+		lblHasta.setBounds(173, 119, 46, 14);
 		getContentPane().add(lblHasta);
 		
 		textField_2 = new JTextField();
@@ -89,105 +91,106 @@ public class generarReporteAlquileres extends JInternalFrame {
 		getContentPane().add(textField_3);
 		textField_3.setColumns(10);
 		
-		JLabel lblFiltros = new JLabel("FILTROS");
-		lblFiltros.setBounds(52, 159, 86, 14);
-		getContentPane().add(lblFiltros);
-		
 		JLabel lblSucursalDestino = new JLabel("Sucursal Destino");
-		lblSucursalDestino.setBounds(10, 285, 99, 14);
+		lblSucursalDestino.setBounds(388, 119, 99, 14);
 		getContentPane().add(lblSucursalDestino);
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(119, 282, 76, 20);
+		comboBox_1.setBounds(492, 91, 96, 20);
 		getContentPane().add(comboBox_1);
 		
 		JLabel lblCliente = new JLabel("CLIENTE");
-		lblCliente.setBounds(279, 195, 76, 14);
+		lblCliente.setBounds(613, 69, 76, 14);
 		getContentPane().add(lblCliente);
 		
 		JLabel lblSucursales = new JLabel("SUCURSALES");
-		lblSucursales.setBounds(47, 195, 91, 14);
+		lblSucursales.setBounds(388, 69, 91, 14);
 		getContentPane().add(lblSucursales);
 		
 		JLabel lblTipoDoc = new JLabel("Tipo Doc");
-		lblTipoDoc.setBounds(219, 239, 65, 14);
+		lblTipoDoc.setBounds(613, 94, 65, 14);
 		getContentPane().add(lblTipoDoc);
 		
 		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setBounds(297, 239, 71, 20);
+		comboBox_2.setBounds(688, 91, 86, 20);
 		getContentPane().add(comboBox_2);
 		
-		JLabel lblNroDoc = new JLabel("nro Doc");
-		lblNroDoc.setBounds(219, 285, 65, 14);
+		JLabel lblNroDoc = new JLabel("Nro Doc");
+		lblNroDoc.setBounds(613, 119, 65, 14);
 		getContentPane().add(lblNroDoc);
 		
 		textField_4 = new JTextField();
-		textField_4.setBounds(289, 282, 86, 20);
+		textField_4.setBounds(688, 116, 86, 20);
 		getContentPane().add(textField_4);
 		textField_4.setColumns(10);
 		
 		JLabel lblVehiculo = new JLabel("VEHICULO");
-		lblVehiculo.setBounds(485, 195, 83, 14);
+		lblVehiculo.setBounds(10, 147, 83, 14);
 		getContentPane().add(lblVehiculo);
 		
 		JLabel lblMarca = new JLabel("Marca");
-		lblMarca.setBounds(422, 242, 46, 14);
+		lblMarca.setBounds(10, 171, 46, 14);
 		getContentPane().add(lblMarca);
 		
 		JLabel lblModelo = new JLabel("Modelo");
-		lblModelo.setBounds(422, 285, 46, 14);
+		lblModelo.setBounds(294, 171, 46, 14);
 		getContentPane().add(lblModelo);
 		
 		JLabel lblColor = new JLabel("Color");
-		lblColor.setBounds(422, 326, 46, 14);
+		lblColor.setBounds(10, 205, 46, 14);
 		getContentPane().add(lblColor);
 		
 		JLabel lblCantPuertas = new JLabel("Cant Puertas");
-		lblCantPuertas.setBounds(373, 361, 108, 14);
+		lblCantPuertas.setBounds(613, 171, 108, 14);
 		getContentPane().add(lblCantPuertas);
 		
 		textField_5 = new JTextField();
-		textField_5.setBounds(482, 239, 86, 20);
+		textField_5.setBounds(52, 171, 86, 20);
 		getContentPane().add(textField_5);
 		textField_5.setColumns(10);
 		
 		textField_6 = new JTextField();
-		textField_6.setBounds(482, 282, 86, 20);
+		textField_6.setBounds(340, 168, 86, 20);
 		getContentPane().add(textField_6);
 		textField_6.setColumns(10);
 		
 		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setBounds(485, 323, 83, 20);
+		comboBox_3.setBounds(52, 202, 86, 20);
 		getContentPane().add(comboBox_3);
 		
 		textField_7 = new JTextField();
-		textField_7.setBounds(482, 358, 86, 20);
+		textField_7.setBounds(703, 168, 71, 20);
 		getContentPane().add(textField_7);
 		textField_7.setColumns(10);
 		
-		JCheckBox chckbxAireAcondicionado = new JCheckBox("AIRE ACONDICIONADO");
-		chckbxAireAcondicionado.setBounds(422, 385, 178, 23);
+		JCheckBox chckbxAireAcondicionado = new JCheckBox("Aire Acondicionado");
+		chckbxAireAcondicionado.setBounds(144, 201, 178, 23);
 		getContentPane().add(chckbxAireAcondicionado);
 		
 		JButton btnGenerar = new JButton("Generar");
-		btnGenerar.setBounds(641, 385, 89, 23);
+		btnGenerar.setBounds(627, 201, 147, 23);
 		getContentPane().add(btnGenerar);
 		
 		JLabel lblTamao = new JLabel("Tama\u00F1o");
-		lblTamao.setBounds(571, 242, 65, 14);
+		lblTamao.setBounds(148, 171, 65, 14);
 		getContentPane().add(lblTamao);
 		
 		JLabel lblTransmision = new JLabel("Transmision");
-		lblTransmision.setBounds(571, 285, 83, 14);
+		lblTransmision.setBounds(436, 171, 83, 14);
 		getContentPane().add(lblTransmision);
 		
 		JComboBox comboBox_4 = new JComboBox();
-		comboBox_4.setBounds(646, 239, 67, 20);
+		comboBox_4.setBounds(198, 168, 86, 20);
 		getContentPane().add(comboBox_4);
 		
 		JComboBox comboBox_5 = new JComboBox();
-		comboBox_5.setBounds(654, 282, 76, 20);
+		comboBox_5.setBounds(512, 168, 76, 20);
 		getContentPane().add(comboBox_5);
-
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 230, 764, 199);
+		getContentPane().add(scrollPane);
+		
+		
 	}
 }
